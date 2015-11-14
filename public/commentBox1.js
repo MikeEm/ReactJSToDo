3833081fa28a08ce94c00107ca030e3f9ef8b788
MikeEm <memilien@elv.telecom-lille.fr>
2015-11-14 21:23:39 +0100

    test
    test

--- public/commentBox1.js
+++ public/commentBox1.js
@@ -2,7 +2,7 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        Hello, world! This is a CommentBox!
       </div>
     );
   }
