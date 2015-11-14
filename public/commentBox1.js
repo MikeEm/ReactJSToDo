de4ef62c8071843cded79d721852e90a72335de0
MikeEm <memilien@elv.telecom-lille.fr>
2015-11-15 00:36:55 +0100

    tests
    add commentbox, commentlist and comments (en dur)

--- public/commentBox1.js
+++ public/commentBox1.js
@@ -2,11 +2,48 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! This is a CommentBox!
+        <h1>Comments</h1>
+        <CommentList/>
+        <CommentForm/>
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+    render: function(){
+        return(
+            <div className = "commentList">
+            <Comment author="Mr. California">I am the King Lizard</Comment>
+            <Comment author="Ron Swanson">I do what I want</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function(){
+        return(
+            <div className = "commentForm">
+            This is a Comment form!
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+   render: function(){
+    return(
+        <div className="comment">
+            <h2 className="commentAuthor">Author</h2>
+            {this.props.author}
+            <h2>Comment</h2>
+            {this.props.children}
+        </div>
+    );
+   }
+});
+
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
